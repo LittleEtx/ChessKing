42563b811e7460bd140c0d5b71af46cf07aa4265
Lumi <47151313+BHAAA-ZLM@users.noreply.github.com>
2022-04-26 15:56:46 +0800

    Changed position of buttons

--- src/main/java/edu/sustech/chessking/ui/LocalStartNewGame.java
+++ src/main/java/edu/sustech/chessking/ui/LocalStartNewGame.java
@@ -1,28 +1,52 @@
 package edu.sustech.chessking.ui;
 
+import com.almasb.fxgl.scene.Scene;
 import com.almasb.fxgl.scene.SubScene;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class LocalStartNewGame extends SubScene {
     public LocalStartNewGame() {
-        Button newGame = new Button("New Game");
-        newGame.setOnAction(event ->{
+        Button newGameBtn = new Button("New Game");
+        newGameBtn.setOnAction(event ->{
             getSceneService().popSubScene();
             getGameController().startNewGame();
         });
-        newGame.getStyleClass().add("subScene-button");
+        newGameBtn.getStyleClass().add("subScene-button");
 
-        StackPane interPane = new StackPane(newGame);
-        interPane.setMaxSize(400,400);
-        interPane.getStyleClass().add("subScene");
+        Button backBtn = new Button("Back");
+        backBtn.setOnAction(event ->{
+           getSceneService().popSubScene();
+        });
+        backBtn.getStyleClass().add("subScene-button");
 
-        StackPane pane = new StackPane(interPane);
-        pane.setPrefSize(getAppWidth(),getAppHeight());
-        pane.setStyle("-fx-background-color: #0007;");
+        HBox buttons = new HBox(50,backBtn,newGameBtn);
+        buttons.setLayoutX(600-125);
+        buttons.setLayoutY(735);
 
-        getContentRoot().getChildren().add(pane);
+        //maybe don't use a rectangle
+        Rectangle rect = new Rectangle(600,400,Color.web("#606060"));
+        rect.setLayoutX(300);
+        rect.setLayoutY(400);
+
+        getContentRoot().getChildren().addAll(rect,buttons);
+//        StackPane newGame = new StackPane(buttons);
+//        newGame.setMaxSize(400,400);
+//        newGame.getStyleClass().add("subScene");
+//
+//
+//        StackPane newGameBackGround = new StackPane(newGame);
+//        newGameBackGround.setPrefSize(getAppWidth(),getAppHeight());
+//        newGameBackGround.setStyle("-fx-background-color: #0007;");
+//
+//        getContentRoot().getChildren().add(newGameBackGround);
     }
 }
