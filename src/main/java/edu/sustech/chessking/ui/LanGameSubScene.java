c91f3318c863108690536738d759e1f8d4b21313
Lumi <47151313+BHAAA-ZLM@users.noreply.github.com>
2022-05-22 00:29:41 +0800

    Local network ui is now much better

--- src/main/java/edu/sustech/chessking/ui/LanGameSubScene.java
+++ src/main/java/edu/sustech/chessking/ui/LanGameSubScene.java
@@ -1,6 +1,7 @@
 package edu.sustech.chessking.ui;
 
 import com.almasb.fxgl.core.serialization.Bundle;
+import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.net.Connection;
 import com.almasb.fxgl.scene.SubScene;
 import edu.sustech.chessking.ChessKingApp;
@@ -12,6 +13,7 @@ import edu.sustech.chessking.gameLogic.multiplayer.Lan.LanServerSearcher;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.effect.Bloom;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -20,14 +22,18 @@ import javafx.scene.shape.Rectangle;
 import java.io.IOException;
 
 import static com.almasb.fxgl.dsl.FXGL.getDialogService;
+import static com.almasb.fxgl.dsl.FXGL.getSceneService;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.getUIFactoryService;
 
 public class LanGameSubScene extends SubScene {
     public LanGameSubScene() {
+        double firstRow = 240d;
+
         Rectangle rect = new Rectangle(1200,800, Color.web("#00000090"));
         getContentRoot().getChildren().addAll(rect);
 
 
-        HBox mainBox = new HBox(20);
+        HBox mainBox = new HBox(30);
         mainBox.setAlignment(Pos.CENTER);
         mainBox.setStyle("-fx-background-radius: 10;" +
                 "-fx-background-color: linear-gradient(from 0.0% 0.0% to 100.0% 0.0%, #193237ff 0.0%, #2e4e58ff 50.0%, #39687cff 100.0%);" +
@@ -35,31 +41,55 @@ public class LanGameSubScene extends SubScene {
         mainBox.setPrefSize(800,600);
         mainBox.setLayoutX(200);
         mainBox.setLayoutY(100);
+        getContentRoot().getChildren().add(mainBox);
 
         VBox btnBox = new VBox(20);
-        btnBox.setAlignment(Pos.TOP_CENTER);
+        btnBox.setAlignment(Pos.CENTER);
+        btnBox.setPrefSize(150,140);
+        btnBox.setLayoutX(firstRow);
+        btnBox.setLayoutY(500);
+
+        var localNetText = getUIFactoryService().newText("Local\nNetwork",Color.WHITE,35);
+        localNetText.setStroke(Color.WHITE);
+        localNetText.setStrokeWidth(3);
+        if(!FXGL.isMobile()){
+            localNetText.setEffect(new Bloom(0.8));
+        }
+        localNetText.setLayoutX(firstRow);
+        localNetText.setLayoutY(200);
+        getContentRoot().getChildren().add(localNetText);
 
         Button newGameBtn = new Button("Create Game");
-        newGameBtn.setLayoutY(50);
         newGameBtn.getStyleClass().add("menu-button");
 
         Button joinBtn = new Button("Join Game");
         joinBtn.getStyleClass().add("menu-button");
 
         btnBox.getChildren().addAll(newGameBtn, joinBtn);
-        mainBox.getChildren().add(btnBox);
+        getContentRoot().getChildren().add(btnBox);
 
         VBox gameBox = new VBox(10);
         gameBox.setPrefWidth(500);
-        gameBox.setStyle("-fx-background-color: transparent");
+        gameBox.setStyle("");
 
         ScrollPane sp = new ScrollPane(gameBox);
-        sp.setStyle("-fx-background-color: transparent");
+        sp.getStyleClass().add("scroll-pane");
         sp.setFitToWidth(true);
         sp.setMaxHeight(500);
-        mainBox.getChildren().add(sp);
+        sp.setMinHeight(500);
+        sp.setLayoutX(450);
+        sp.setLayoutY(150);
+        getContentRoot().getChildren().add(sp);
 
-        getContentRoot().getChildren().add(mainBox);
+        Button backBtn = new Button();
+        backBtn.getStyleClass().add("backBtn");
+        backBtn.setOnAction(event -> {
+            getSceneService().popSubScene();
+        });
+        backBtn.setLayoutX(950);
+        backBtn.setLayoutY(100);
+
+        getContentRoot().getChildren().add(backBtn);
     }
 
     @Override
